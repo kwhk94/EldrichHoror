dfad229ffc6ac34e80ec3c39d3507aa0aa940f48
kwhk94 <kwhk1993@naver.com>
2017-11-30 12:51:31 +0900

    3d빌더로 지역 메쉬제작
    지역 메쉬 제작하여 피킹 가능하게 해서 캐릭터가 이동하게 하려고함
    네비 메쉬도 bake가능

---
+++ Unity/Eldrich_horror/Assets/Script/Location.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Location : MonoBehaviour {
+    public test_nav player;
+    
+ 
+}
---
+++ Unity/Eldrich_horror/Assets/Script/test_nav.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class test_nav : MonoBehaviour {
+
+    public Transform target_pos;
+    public NavMeshAgent agent;
+    public RaycastHit hit;
+
+    private int LocationMask;
+    
+    void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        LocationMask = LayerMask.GetMask("Location");
+    }
+	
+	// Update is called once per frame
+	void Update() {       
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, LocationMask))
+            {                
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}

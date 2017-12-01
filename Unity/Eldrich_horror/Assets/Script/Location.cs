0944ffa0a792cf70f884dc2d8aa374e5bc9f3ff4
kwhk94 <kwhk1993@naver.com>
2017-12-01 18:09:47 +0900

    카메라 시스템, 장소 메쉬제작
    장소 메쉬는 겉의 띠를 밝게 보이기 위해, 그리고 게임상에서 장소를 마우스로 가져다 대면 경로와 장소를 눈에 띄게 하기 위해 만들었다.
    카메라는 이동과 줌인 줌아웃을 코드화함

--- Unity/Eldrich_horror/Assets/Script/Location.cs
+++ Unity/Eldrich_horror/Assets/Script/Location.cs
@@ -3,8 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+public enum Type_loc { Main_green = 0x1, Main_orange = 0x2, Main_purple = 0x4, Main_other = 0x8 ,Main_sub = 0x80};
+public enum Type_sub {sub_city = 0x10, sub_jungle = 0x20, sub_sea = 0x40 };
+
+
+
 public class Location : MonoBehaviour {
-    public test_nav player;
-    
- 
+    public MeshRenderer [] Location_Path;    
+
+    public Type_loc MainType;
+    public Type_sub SubType;
+    private void OnMouseEnter()
+    {
+        for(int i=0;i<Location_Path.Length ;++i)
+        {
+            //마우스가 올려지면 경로를 보여준다.
+            Location_Path[i].enabled = true;
+        }
+    }
+    private void OnMouseExit()
+    {
+        for (int i = 0; i < Location_Path.Length; ++i)
+        {
+            //마우스가 올려지면 경로를 꺼버린다.
+            Location_Path[i].enabled = false;
+        }
+    }
+
+   
 }

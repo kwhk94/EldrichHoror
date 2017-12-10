22ae095824c73b62128e2f917510b1c304ee9efe
kwhk94 <kwhk1993@naver.com>
2017-12-10 16:53:07 +0900

    UI를 화면 비율에 맞게 변경
    비율에 맞게 UI 조정

--- Unity/Eldrich_horror/Assets/PlayerUI.cs
+++ Unity/Eldrich_horror/Assets/PlayerUI.cs
@@ -5,11 +5,11 @@ using UnityEngine.UI;
 
 public class PlayerUI : Photon.PunBehaviour,IPunObservable {
 	public List<Sprite> imagelist;
+	private RectTransform rectTransform;
 	private PhotonView photonview;
 	private Image image;
 	void Start () {
-		photonview = GetComponent<PhotonView> ();		
-		image = GetComponent<Image> ();
+		SettingComponent ();
 		//플레이어번호와 캐릭터 번호,이름을 넘긴다
 		if (photonview.isMine) {
 			int num = LobbyPlayerlist.Instance.current_player_number;
@@ -17,8 +17,15 @@ public class PlayerUI : Photon.PunBehaviour,IPunObservable {
 			string playername = PhotonNetwork.player.NickName;
 			photonview.RPC ("SetPlayerUI", PhotonTargets.All, num, chracter_num, playername);
 		}
-
 	}
+
+	private void SettingComponent()
+	{
+		photonview = GetComponent<PhotonView> ();		
+		image = GetComponent<Image> ();
+		rectTransform = GetComponent<RectTransform> ();
+	}
+
 
 	public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
 	{
@@ -35,10 +42,16 @@ public class PlayerUI : Photon.PunBehaviour,IPunObservable {
 	[PunRPC]
 	public void SetPlayerUI(int num,int chracternum,string playername)
 	{
+		if (!image)
+			SettingComponent ();
 		transform.parent = GameObject.Find ("Panel").transform;
+		image.sprite = imagelist [chracternum];
+		GetComponentInChildren<Text>().text = playername;
+		rectTransform.anchorMax = new Vector2 (0.05f, 0.1f*num+0.4f);
+		rectTransform.anchorMin = new Vector2 (0.0f, 0.1f*num+0.3f);
+		rectTransform.pivot = new Vector2 (0.5f, 0.5f);
+		rectTransform.offsetMin = new Vector2(0.0f, 0.0f);
+		rectTransform.offsetMax = new Vector2(-0.0f, -0.0f);
 
-		GetComponent<Image>().sprite = imagelist [chracternum];
-		GetComponent<RectTransform> ().position += new Vector3 (0, -80 * num, 0);
-		GetComponentInChildren<Text> ().text = playername;
 	}
 }
